263ed37e0bdd9c67e22529f5b4324bf5ba84e612
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2025-06-01 13:05:17 +0000

    Update nocmt to 1.1.5

--- Formula/nocmt.rb
+++ Formula/nocmt.rb
@@ -1,15 +1,15 @@
 class Nocmt < Formula
   desc "Tool for removing comments from source code while preserving structure"
   homepage "https://github.com/2mawi2/nocmt"
-  url "https://github.com/2mawi2/nocmt/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "c9c94f00d7f631d682f242727ab092501ad3b7c4f83b159d3c5267a39b3e4ab4"
+  url "https://github.com/2mawi2/nocmt/archive/refs/tags/v1.1.5.tar.gz"
+  sha256 "00202f86dbc1a8d383a96d004962accbb08f9d7dacec626888d407f09cafe7d3"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "1"
-    system "go", "build", *std_go_args(ldflags: "-s -w -X nocmt/internal/cli.Version=1.1.4"), "./cmd/nocmt"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X nocmt/internal/cli.Version=1.1.5"), "./cmd/nocmt"
   end
 
   test do
