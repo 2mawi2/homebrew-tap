2ef8cda8b401e36ea84f64efb08e937281e2b476
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2025-06-01 15:05:40 +0000

    Update sw to 0.1.3

--- Formula/sw.rb
+++ Formula/sw.rb
@@ -1,8 +1,8 @@
 class Sw < Formula
   desc "A fast project switcher for developers"
   homepage "https://github.com/2mawi2/switchr"
-  url "https://github.com/2mawi2/switchr/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "ccc508c82ac1b0eecb4cc8484979abce9613b51626e492e55b5803dcec3b2667"
+  url "https://github.com/2mawi2/switchr/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "097a143cb0658ff700fc51db1cdb03fdfa59a2abfad73e569d882187cc92e74f"
   license "MIT"
 
   depends_on "rust" => :build
