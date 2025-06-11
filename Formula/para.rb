71d398000ab563234b1034b8d335449dde8b7dda
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2025-06-11 09:22:21 +0000

    Update para to 1.1.1

--- Formula/para.rb
+++ Formula/para.rb
@@ -1,27 +1,33 @@
 class Para < Formula
   desc "Parallel IDE workflow helper for Git worktrees"
   homepage "https://github.com/2mawi2/para"
-  url "https://github.com/2mawi2/para/archive/refs/tags/v1.0.28.tar.gz"
-  sha256 "14c75d1e143f53054fd8f5dc6be039833ab59e108bbe04bd164a4556cd405a39"
+  url "https://github.com/2mawi2/para/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "235dc5ef17563f99497a1c42a813ab8b38666b529114f865b0eac4e419c19e65"
   license "MIT"
-  version "1.0.28"
+
+  depends_on "rust" => :build
 
   def install
-    # Install the para script and libraries
-    libexec.install "para.sh"
-    libexec.install "lib"
-    
-    # Create wrapper script
-    (bin/"para").write <<~EOS
-      #!/usr/bin/env sh
-      exec "#{libexec}/para.sh" "$@"
+    system "cargo", "install", *std_cargo_args
+  end
+
+  def caveats
+    <<~EOS
+      To enable shell completion, add this to your shell config:
+      
+      For bash:
+        echo 'eval "$(para completion bash)"' >> ~/.bashrc
+      
+      For zsh:
+        echo 'eval "$(para completion zsh)"' >> ~/.zshrc
+      
+      For fish:
+        para completion fish | source
     EOS
-    
-    # Make wrapper executable
-    chmod 0755, bin/"para"
   end
 
   test do
+    assert_match "para", shell_output("#{bin}/para --version")
     system "#{bin}/para", "--help"
   end
 end
